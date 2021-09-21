22f27dfd9ebc92d51eb7407d70db6c7dba300666
Wellington Gadelha <contato.informeai@gmail.com>
2021-09-21 19:54:42 -0300

    add router and create interface for shorten page.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # :link:Shorten
 Link shortening project.
 
+### Install  
+Get Project.
+```
+go get github.com/informeai/shorten
+```
 
 
 by wellington gadelha :+1:
--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+//ShortLink struct of return links shortening.
+type ShortLink struct {
+	Short string `json:"shortlink"`
+}
+
+//LongUrl struct of parse request url data.
+type LongUrl struct {
+	Long string `json:"long"`
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	fBytes, err := ioutil.ReadFile("./templates/index.html")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+	w.Write(fBytes)
+}
+func shortLinks(w http.ResponseWriter, r *http.Request) {
+	var long LongUrl
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&long)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//create shortlink here...
+	short := ShortLink{Short: "www.informeai.com.br"}
+	jsonBytes, err := json.Marshal(short)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Write(jsonBytes)
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", serveIndex).Methods("GET")
+	router.HandleFunc("/short", shortLinks).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
